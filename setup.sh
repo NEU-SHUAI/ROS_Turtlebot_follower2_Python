98dc8fb0773eb7c053c84f9091370ac2d813421d
Jon <jjones646@gmail.com>
2015-12-04 23:54:32 -0500

    adding setup script and moving around the source files to reflect the changes

--- setup.sh
+++ setup.sh
@@ -2,14 +2,42 @@
 
 # Script to setup a catkin workspace with the repo files
 
-SCRIPT_DIR="$(cd "$(dirname"${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # source the environment
 . /opt/ros/indigo/setup.bash
 
 # go to repo's directory
-pushd "$SCRIPT_DIR/turtlebot_gt"
+pushd "$SCRIPT_DIR/src" &> /dev/null
 
-# setup the source files
-catkin_init_workspace
+# setup the source files if necessary
+if ! catkin_init_workspace &> /dev/null; then
+    echo "--  no catkin initialization needed"
+fi
 
+pushd .. &> /dev/null
+echo "--  attempting to build package"
+# any failure after now ends the script
+set -e
+catkin_make
+
+# blank line after build output
+echo ""
+
+# try to make the package and ask to
+# add the sourcing env to the user's 
+# bashrc file
+if ! grep -q " $(readlink -m $(pwd)/devel/setup.bash)" ~/.bashrc-test; then
+    read -r -p "Would you like add the env to ~/.bashrc? [y/N] " response
+    case $response in
+        [yY][eE][sS]|[yY]) 
+            echo ". $(readlink -m $(pwd)/devel/setup.bash)" >> ~/.bashrc-test
+            echo "--  added new ros env to ~/.bashrc"
+        ;;
+    *)
+        :
+        ;;
+    esac
+fi
+
+exit 0
